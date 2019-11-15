5b90962a056a25ed68d03dcc642623363c476a4b
JoeVilca <49346518+JoeVilca@users.noreply.github.com>
2019-11-15 00:00:29 -0500

    Update ClasePrueba.java
    prueba

--- src/ClasePrueba.java
+++ src/ClasePrueba.java
@@ -4,6 +4,6 @@
  */
 public class ClasePrueba {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo FISI saludos");
+        System.out.println("Hola Mundo FISI saludos a todos");
     }
 }
